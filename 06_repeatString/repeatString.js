edeb0a4b50c51ab56c4b232791a1dd35a25c7ac6
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-09-18 22:24:55 -0500

    Completed 06 repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = '';
+    if (num < 0) {
+      finalString = 'ERROR';  
+    } else {
+      while (num > 0) {
+        finalString += string;
+        num--;
+      };   
+    };
+    return finalString; 
 };
 
 // Do not edit below this line
