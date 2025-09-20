3767187749642d5b2d4307c19ed6eb21a0ce3e49
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-09-19 21:16:09 -0500

    Work on 08 removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item) {
+    const removed = arr.filter((element) => element !== item);
+    return removed;
 };
-
+//console.table(removeFromArray([1, 2, 3, 4, 5], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
