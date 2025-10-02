81331c549c07db096eaf277c4a9eee132502d3c5
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-10-01 22:01:50 -0500

    Work on 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(low, high) {
+    let total = 0;
+    if (low < 0 || high < 0) {
+        return 'ERROR';
+    };
+    for (let index = low; index <= high; index++) {
+    total += index;
+    };
+    return total;
 };
 
 // Do not edit below this line
