d6aeb7fcd17f6880a1b7a9ef4b7a89aae5dd731c
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-10-02 22:22:49 -0500

    Complete 09sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,10 +1,22 @@
 const sumAll = function(low, high) {
     let total = 0;
+    // check if arguments are integers
+    if (!Number.isInteger(low) || !Number.isInteger(high)) {
+        return'ERROR';
+    };
+    // check if arguments are negative numbers
     if (low < 0 || high < 0) {
         return 'ERROR';
     };
+    // check if high value comes first
+    if (low > high) {
+        let newHigh = low;
+        low = high;
+        high = newHigh;
+    };
+    // iterate through range and sum total
     for (let index = low; index <= high; index++) {
-    total += index;
+        total += index;
     };
     return total;
 };
