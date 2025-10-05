5243bdda57a1d3a9fe595132787f2bddbaf3e766
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-10-04 21:16:20 -0500

    Complete exercise 10 leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
-    if(year % 4 === 0 &&
-        (year % 100 === 0 || year % 400 === 0)) {
+    if((year % 4 === 0 && year % 100 !== 0) //year divides by 4 but not by 100
+        || year % 400 === 0) { //or year divides by 400
         return true;
     } else {
         return false;
